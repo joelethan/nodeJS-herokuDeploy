a5e9179223bd8b39ae1ba05ba8d35efe78f861bd
joelethan <joelkatsme@gmail.com>
2020-02-13 17:26:51 -0800

    test heroku environment variable

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
+require('dotenv').config()
 
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello world'});
+    res.send({ message: `Hello world ${process.env.test_environ}`});
 });
 
 app.listen(port, () => {
